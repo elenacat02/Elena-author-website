3d0cb2d835222a14d40e274e4fbf1d729d2411f0
Bob Carmichael <bobc@edfinity.com>
2024-10-27 07:12:53 -0400

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import eslintPluginPrettier from "eslint-plugin-prettier"
+import eslintConfigPrettier from "eslint-config-prettier"
+
+export default [
+  {
+    files: ["src/**/*.{js,jsx,ts,tsx}"],
+    ignores: [
+      "src/dist",
+      "postcss.config.js",
+      "tailwind.config.js",
+      "vite.config.js",
+    ],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  pluginJs.configs.recommended,
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      ...eslintConfigPrettier.rules,
+      "prettier/prettier": "error",
+    },
+  },
+]
