c2bd1a060f5a7a0ceb9d6406603b15afac7927f9
Bob Carmichael <bobc@edfinity.com>
2024-10-30 07:28:43 -0400

    prep for github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,9 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   root: "src",
+  base: "/Elena-author-website",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
 })
