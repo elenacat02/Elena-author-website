dda2ffc870a115cbbe653c396034e45377f5c58f
Bob Carmichael <bobc@edfinity.com>
2024-11-02 12:30:58 -0400

    fix path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig(({ mode }) => ({
   root: "src",
-  base: mode === "production" ? "/Elena-author-website/" : "/",
+  base: "/",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
